8e44b02ab6a2865d2568873fe0335a5f063b6dc8
mckadesorensen <dmsorensen@alaska.edu>
2020-09-01 19:33:20 -0800

    Created the quicksort header file

---
+++ quicksort.h
@@ -0,0 +1,12 @@
+//
+// Created by Douglas Sorensen on 9/1/20.
+//
+
+#ifndef HOMEWORK_1_QUICKSORT_H
+#define HOMEWORK_1_QUICKSORT_H
+
+#include <vector>
+
+void quicksort(std::vector<double> data);
+
+#endif //HOMEWORK_1_QUICKSORT_H
